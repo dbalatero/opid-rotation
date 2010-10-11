16751252e52dc0a164cbbdd10e61e95867812357
David Balatero <dbalatero@gmail.com>
2010-10-10 18:01:01 -0700

    added chair inex

--- rotation.rb
+++ rotation.rb
@@ -6,7 +6,6 @@ get "/" do
   @rotation = %w(David Ivan Rob Evan Jared)
   start_time = Time.mktime(2010, 10, 4, 0, 0, 0)
   seconds_per_week = 7 * 24 * 60 * 60
-  diff = ((Time.now - start_time) / seconds_per_week.to_f).floor
-
-
+  weeks_since_first = ((Time.now - start_time) / seconds_per_week.to_f).floor
+  @chair_index = weeks_since_first % 5
 end
