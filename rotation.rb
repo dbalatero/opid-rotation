62486e411278ec49942649703e14d571d0149d2d
David Balatero <dbalatero@gmail.com>
2010-10-20 10:48:51 -0700

    Finished rotation app.

--- rotation.rb
+++ rotation.rb
@@ -1,11 +1,18 @@
 require 'rubygems'
 require 'sinatra'
-
+require 'haml'
 
 get "/" do
-  @rotation = %w(David Ivan Rob Evan Jared)
-  start_time = Time.mktime(2010, 10, 4, 0, 0, 0)
+  @names = %w(David Jared Evan Rob Ivan)
+  start_time = Time.mktime(2010, 10, 11, 0, 0, 0)
   seconds_per_week = 7 * 24 * 60 * 60
   weeks_since_first = ((Time.now - start_time) / seconds_per_week.to_f).floor
-  @chair_index = weeks_since_first % 5
+  @chair_index = (weeks_since_first % 5) + 5
+
+  # meeting day
+  @days_until_sunday = 7 - Time.now.wday
+  seconds_until_sunday = @days_until_sunday * 3600 * 24
+  @meeting_day = Time.now + seconds_until_sunday
+
+  haml :index
 end
